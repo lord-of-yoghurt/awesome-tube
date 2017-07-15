b79e70857f9ef14b7245b9e2cd753d896a0ab352
David Ordovskiy <david.ordovsky@gmail.com>
2017-07-15 17:55:02 -0400

    basic component styling, add search

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const KEY = 'AIzaSyC7NfBtNQBvxCtvzd1LxLFZgz4Wf7gsngc'
 
@@ -11,19 +12,31 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
-    YTSearch({key: KEY, term: 'bullshittery'}, (videos) => {
-      // this.setState({ videos: videos })
-      this.setState({ videos })
+    this.videoSearch('bullshittery')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     })
   }
 
   render () {
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={ this.state.videos }/>
+        <SearchBar onSearchTermChange={ term => this.videoSearch(term) } />
+        <VideoDetail video={ this.state.selectedVideo }/>
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={ this.state.videos }/>
       </div>
     )
   }
